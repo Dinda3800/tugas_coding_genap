363164205dfbc0a142060ffc144e9f5d88080174
Dinda3800 <143859405+Dinda3800@users.noreply.github.com>
2023-09-08 19:43:17 +0700

    Add files via upload

---
+++ tugas.py
@@ -0,0 +1,25 @@
+#soal 1
+
+print (" operasi mencari volume balok ")
+p = int(input("panjang = "))
+l = int(input("lebar = "))
+t = int(input("tinggi = "))
+volume = p*l*t
+print ("hasil dari volume balok = ", volume)
+
+#soal 2
+
+print (" operasi mencari volume tabung ")
+r = int(input("jari-jari = "))
+t = int(input("tinggi = "))
+v = 22/7
+volume = 22/7*r*r*t
+print ("hasil dari volume tabung = ", volume)
+
+#soal 3
+
+print (" operasi mencari volume bola ")
+r = int(input("jari-jari = "))
+v = 22/7
+volume = 4/3*22/7*r*r*r
+print ("hasil dari volume bola = ", volume)
